d53c4890d41e76d3d760babbdbfd859f4142759b
juryokun <capeta.kozo@gmail.com>
2022-08-01 00:33:30 +0900

    fix mac setting

--- init/setup_commands/fish_plugins_setup.sh
+++ init/setup_commands/fish_plugins_setup.sh
@@ -5,6 +5,7 @@ ln -snfv $ROOT/config.fish ~/.config/fish/config.fish
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 fisher install jethrokuan/fzf
-fisher install jethrokuan/z
 fisher install 0rax/fish-bd
 fisher install oh-my-fish/theme-bobthefish
+decors/fish-ghq
+# fisher install jethrokuan/z
