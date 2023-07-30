2c6f002f55f124b41970fa57b91ec51b1956f6fc
juryokun <capeta.kozo@gmail.com>
2023-07-30 14:16:47 +0900

    fix astronvim

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -1,5 +1,22 @@
+-- hop.nvim
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', ',f', function()
+  hop.hint_char1()
+end, {remap=true})
+vim.keymap.set('', ',F', function()
+  hop.hint_char2()
+end, {remap=true})
+vim.keymap.set('', ',.', function()
+  hop.hint_patterns()
+end, {remap=true})
+vim.keymap.set('', ',/', function()
+  hop.hint_patterns({}, vim.api.nvim_exec('echo @/', true))
+end, {remap=true})
+
+
 return {
   n = {
-    ["<leader>,"] = {"<cmd>source $MYVIMRC<CR>", desc = "Reload Config"}
-  }
+    ["<leader>,"] = {"<cmd>source $MYVIMRC<CR>", desc = "Reload Config"},
+  },
 }
