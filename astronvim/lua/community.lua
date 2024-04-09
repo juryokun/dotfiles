258f64caaf60c5d780229fff88d9b6a8ed16fe67
juryokun <capeta.kozo@gmail.com>
2024-04-09 20:08:58 +0900

    version up astronvim

---
+++ astronvim/lua/community.lua
@@ -0,0 +1,34 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  {
+    "nvim-surround",
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "<leader>s",
+        visual_line = "<leader>gs",
+        delete = "ds",
+        change = "cs",
+      },
+    },
+  },
+  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  { import = "astrocommunity.colorscheme.sonokai" },
+  { import = "astrocommunity.motion.marks-nvim" },
+  -- import/override with your plugins folder
+}
