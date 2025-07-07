7486c8492354a32a7c246d50335e3bc13aa2f4db
juryokun <capeta.kozo@gmail.com>
2025-07-07 14:06:25 +0900

    add find node

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -3,3 +3,18 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+
+local treeutils = require "custom.plugins.nvim-tree.treeutils"
+map("n", "<Leader>fd", function()
+  treeutils.launch_find_files()
+end)
+map("n", "<Leader>fl", function()
+  treeutils.launch_live_grep()
+end)
